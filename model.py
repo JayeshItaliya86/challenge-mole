c1ccaf737062f3d1ba0c4e9d66f1cad19ca6d5e0
Roldan87 <jose.roldan1987@gmail.com>
2021-09-21 12:12:02 +0200

    model cnn

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ train_img, val_img, train_labels, val_labels = train_test_split(train_val_img, t
 #CNN Model
 
 # pixel width and height of our images
-input_size = (600,450)
+input_size = (100,125)
 # number of filters in the convnet layer
 filters = 64
 # conv net parameters
@@ -59,9 +59,25 @@ kernel_size = (5, 5)
 
 # Create the first version of the model
 model = tf.keras.Sequential()
-model.add(layers.Conv2D(filters=filters, kernel_size=kernel_size, input_shape=(600,450,3), activation='relu'))
+model.add(layers.Conv2D(filters=filters, kernel_size=kernel_size, input_shape=(100,125,3), activation='relu'))
 model.add(layers.Flatten())
-model.add(layers.Dense(2, activation='sigmoid'))
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(train_img, train_labels, epochs=6, batch_size=256)
+model.add(layers.Dense(2, activation='softmax'))
+model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False), optimizer='adam', metrics=['accuracy'])
+model.fit(train_img, train_labels, epochs=10, batch_size=512)
 model.summary()
+
+#Prediction and Conf Matrix
+pred = model.predict('test')
+matrix = tf.math.confusion_matrix(labels=tf.argmax('true_labels', 1), predictions=tf.argmax('pred', 1))
+
+#Plot Conf Matrix
+
+ax= plt.subplot()
+sns.heatmap(matrix, annot=True, fmt='g', ax=ax)
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels') 
+ax.set_title('Confusion Matrix') 
+ax.xaxis.set_ticklabels(['not_cancer', 'cancer'])
+ax.yaxis.set_ticklabels(['not_cancer', 'cancer'])
+plt.show()
